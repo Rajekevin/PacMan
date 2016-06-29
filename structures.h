275ee3728ae7f486a46b91d1e67476be5f494988
Rajekevin <rajekevin@hotmail.fr>
2016-06-29 12:32:37 +0200

    map begin now

---
+++ structures.h
@@ -0,0 +1,20 @@
+//
+//  structures.h
+//  pacmanc
+//
+//  Created by Elyes on 21/06/2016.
+//  Copyright © 2016 Elyes. All rights reserved.
+//
+
+#ifndef structures_h
+#define structures_h
+
+typedef struct coordonees coordonees;
+
+struct coordonees
+{
+    int i;
+    int j;
+};
+
+#endif /* structures_h */
