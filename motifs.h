164ba30e79fe84e0bb44414be98a5967c4009ad1
Rajekevin <rajekevin@hotmail.fr>
2016-07-13 17:18:57 +0200

    Symbole added

---
+++ motifs.h
@@ -0,0 +1,22 @@
+#ifndef MOTIFS_H
+#define MOTIFS_H
+
+
+
+
+
+#define coin_hg (char)0xC9 //coin haut gauche
+#define coin_hd (char)0xBB //coin haut droit
+#define coin_bg (char)0xC8 //coin bas gauche
+#define coin_bd (char)0xBC //coin bas droit
+#define bar_hor (char)0xCD //barre horizontale
+#define bar_ver (char)0xBA //barre verticale
+#define t_gau (char)0xB9 //T vers la gauche
+#define t_hau (char)0xCA //T vers le haut
+#define t_bas (char)0xCB //T vers le bas
+#define t_dro (char)0xCC //T vers la droite
+#define croix (char)0xCE //barre verticale
+#define point (char)0x4 //Points bonus
+
+
+#endif // MOTIFS_H
