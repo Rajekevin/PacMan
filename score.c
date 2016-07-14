8a56a6c6066b5e7220384887d2705d41e7ef367f
Rajekevin <rajekevin@hotmail.fr>
2016-07-14 16:13:26 +0200

    Gere le score du Pacman

---
+++ score.c
@@ -0,0 +1,36 @@
+#include "score.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "subscribeScore.h"
+#include "triScore.h"
+#include "ReadScore.h"
+
+void fscore(int scorePlayer)
+{
+    int score[100]={0}, maxi, i;
+    char name[12];
+    char pseudo[100][4];
+
+    printf("Votre Pseudo (3 caracteres): ");
+    //Saisie du pseudo
+    do
+    {
+        gets(name);
+
+    }while (strlen(name)!=3);
+
+    //Passage en majuscule
+    for (i=0 ; i<4 ; i++)
+    {
+        name[i]=toupper(name[i]);
+    }
+
+
+    printf("\n\nVotre score est: %d\n\n", scorePlayer);
+    subscribeScore(name, scorePlayer);    //Inscription du score dans le fichier .txt
+    maxi = ReadScore(pseudo, score);     //
+    triScore(pseudo, score, maxi);
+}
---
+++ score.h
@@ -0,0 +1,7 @@
+#ifndef SCORE_H_INCLUDED
+#define SCORE_H_INCLUDED
+
+void fscore(int );
+
+
+#endif // SCORE_H_INCLUDED
